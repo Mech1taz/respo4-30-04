c6de77944ee19ef1b75cdc63cb9ac6f8b6406ee8
MechitaZ <ma.astudillom@duocuc.cl>
2025-04-30 15:12:51 -0400

    HABIAN COSAS DE MAS

--- src/main/java/com/example/respo4/controller/PersonaController.java
+++ src/main/java/com/example/respo4/controller/PersonaController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.respo4.model.Persona;
 import com.example.respo4.service.PersonaService;
 
-import jakarta.websocket.server.PathParam;
 
 import java.util.List;
 
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -39,4 +37,5 @@ public class PersonaController {
     public Persona getPersonaId(@PathVariable int id){
         return personaService.buscarxId(id);
     }
+
 }
